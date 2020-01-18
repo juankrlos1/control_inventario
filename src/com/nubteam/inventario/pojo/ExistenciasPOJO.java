01181f7dc8ad855c7a826258c9ca9af808a1966c
Franklin0817 <ramirezandres1724@gmail.com>
2020-01-18 02:10:33 -0600

    Se actualiza la imformacion de las existencias

---
+++ src/com/nubteam/inventario/pojo/ExistenciasPOJO.java
@@ -0,0 +1,35 @@
+package com.nubteam.inventario.pojo;
+
+public class ExistenciasPOJO {
+	
+	private String nombre;
+	private String tipo;
+	private String can_disp;
+	
+	public ExistenciasPOJO() {}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getCan_disp() {
+		return can_disp;
+	}
+
+	public void setCan_disp(String can_disp) {
+		this.can_disp = can_disp;
+	}
+	
+}
